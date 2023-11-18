f7b36020f6990253faff15de22205cfc8a4a5943
Abraham Wilfried Nguega Yepseu <awngy@ulaval.ca>
2023-11-18 06:06:38 -0500

    test2

--- phase1.py
+++ phase1.py
@@ -11,31 +11,3 @@ def analyser_commande():
     parser.add_argument('-v', '--valeur', choices=['fermeture', 'ouverture', 'min', 'max', 'volume'], default='fermeture', help='La valeur désirée (par défaut: fermeture)')
     return parser.parse_args()
 
-def produire_historique(symbole, date_debut, date_fin, valeur_desiree):
-    url = f'https://pax.ulaval.ca/action/{symbole}/historique/'
-    params = {
-        'début': date_debut,
-        'fin': date_fin,
-    }
-    
-    response = requests.get(url=url, params=params)
-    
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        historique = data.get('historique', {})
-        result = [(date, values[valeur_desiree]) for date, values in historique.items()]
-        return result
-    else:
-        print(f"Erreur lors de la récupération des données pour {symbole}: {response.text}")
-        return []
-
-if __name__ == '__main__':
-    args = analyser_commande()
-
-    for symbole in args.symboles:
-        date_debut = args.début or args.fin  # Utilise la date de fin par défaut si la date de début n'est pas spécifiée
-        date_fin = args.fin or str(datetime.date.today())  # Utilise la date d'aujourd'hui par défaut si la date de fin n'est pas spécifiée
-
-        historique = produire_historique(symbole, date_debut, date_fin, args.valeur)
-        print(f"titre={symbole}: valeur={args.valeur}, début={date_debut}, fin={date_fin}")
-        print(historique)
