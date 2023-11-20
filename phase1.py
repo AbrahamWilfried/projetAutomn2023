0e92b374f7362ff93270704ef12ad6640b03e145
Abraham Wilfried Nguega Yepseu <awngy@ulaval.ca>
2023-11-19 23:07:23 -0500

    test8

--- phase1.py
+++ phase1.py
@@ -1,19 +1,21 @@
-import argparse
+import argparse  #module standard de python requise pour le projet
 import json
 import datetime
 import requests
 
 def analyser_commande():
+    # fonction qui encapsule tous les appels au module argparse
     parser = argparse.ArgumentParser(description="Extraction de "
     "valeurs historiques pour un ou plusieurs symboles boursiers.")
     parser.add_argument('symbole', nargs='+', help='Nom de symbole boursiers')
     parser.add_argument('-d', '--début', type=str, help='Date de début (format: AAAA-MM-JJ)')
     parser.add_argument('-f', '--fin', type=str, help='Date de fin (format: AAAA-MM-JJ)')
-    parser.add_argument('-v', '--valeur', choices=['fermeture', 'ouverture', 'min', 'max', 
+    parser.add_argument('-v', '--valeur', choices=['fermeture', 'ouverture', 'min', 'max',
     'volume'],default='fermeture', help='La valeur désirée (par défaut: fermeture)')
     return parser.parse_args()
 
 def produire_historique(symboles, début, fin, valeur_desiree):
+    #fonction qui interagir avec le serveur du cours afin de récupérer l'historique des symboles désirés
     url = f'https://pax.ulaval.ca/action/{symboles}/historique/'
     params = {
         'début': début,
@@ -32,7 +34,7 @@ if __name__ == '__main__':
     for symbole in args.symbole:
         date_début = args.début or args.fin
         date_fin = args.fin or str(datetime.date.today())
-        historique = produire_historique(symbole, date_début, date_fin, args.valeur)
+        historiques = produire_historique(symbole, date_début, date_fin, args.valeur)
         print(f"titre={symbole}: valeur={args.valeur}, début={date_début}, fin={date_fin}")
-        print(historique)
+        print(historiques)
         
