666e4397112b0904dcfb66caf30c49d97d766d59
Abraham Wilfried Nguega Yepseu <awngy@ulaval.ca>
2023-11-19 22:50:06 -0500

    test6

--- phase1.py
+++ phase1.py
@@ -4,19 +4,19 @@ import datetime
 import requests
 
 def analyser_commande():
-    parser = argparse.ArgumentParser(description="Extraction de " 
+    parser = argparse.ArgumentParser(description="Extraction de "
     "valeurs historiques pour un ou plusieurs symboles boursiers.")
-    parser.add_argument('symboles', nargs='+', help='Nom des symboles boursiers')
+    parser.add_argument('symbole', nargs='+', help='Nom de symbole boursiers')
     parser.add_argument('-d', '--début', type=str, help='Date de début (format: AAAA-MM-JJ)')
     parser.add_argument('-f', '--fin', type=str, help='Date de fin (format: AAAA-MM-JJ)')
-    parser.add_argument('-v', '--valeur', choices=['fermeture', 'ouverture', 'min', 'max', 'volume'],
-                        default='fermeture', help='La valeur désirée (par défaut: fermeture)')
+    parser.add_argument('-v', '--valeur', choices=['fermeture', 'ouverture', 'min', 'max', 
+    'volume'],default='fermeture', help='La valeur désirée (par défaut: fermeture)')
     return parser.parse_args()
 
-def produire_historique(symboles, debut, fin, valeur_desiree):
+def produire_historique(symboles, début, fin, valeur_desiree):
     url = f'https://pax.ulaval.ca/action/{symboles}/historique/'
     params = {
-        'début': debut,
+        'début': début,
         'fin': fin,
     }
     response = requests.get(url=url, params=params)
@@ -29,9 +29,10 @@ def produire_historique(symboles, debut, fin, valeur_desiree):
     return []
 if __name__ == '__main__':
     args = analyser_commande()
-    for symboles in args.symboles:
-        debut = args.début or args.fin  
-        fin = args.fin or str(datetime.date.today())  
-        historique = produire_historique(symboles, debut, fin, args.valeur)
-        print(f"titre={symboles}: valeur={args.valeur}, début={debut}, fin={fin}")
+    for symbole in args.symbole:
+        date_début = args.début or args.fin
+        date_fin = args.fin or str(datetime.date.today())
+        historique = produire_historique(symboles, date_début, date_fin, args.valeur)
+        print(f"titre={symboles}: valeur={args.valeur}, début={date_début}, fin={date_fin}")
         print(historique)
+        
